fc8aeebd4f2542768bbcd87cd3f428a78f8cfdbe
James Hilts <jameshilts@icloud.com>
2021-12-01 17:00:01 -0800

    Added all project folders.

--- index.js
+++ index.js
@@ -39,7 +39,6 @@ app.use(bodyParser.json());
 app.use(fileUpload({createParentPath: true}))
 app.use(bodyParser.urlencoded({extended: true}))
 
-
 app.get('/', (req, res)=> getAllArticles.handleAllArticles(req, res, db))
 app.post('/signup', (req, res)=> signup.handleSignup(req, res, db, bcrypt))	
 app.post('/login', (req, res)=> login.handleLoginAuth(req, res, db, bcrypt))
