d99fc33f70f751524452e228fec18d4dbe4a51d7
James Hilts <jameshilts@icloud.com>
2021-12-13 18:01:13 -0800

    Fixing cors issues.

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ app.use(bodyParser.json());
 app.use(fileUpload({createParentPath: true}))
 app.use(bodyParser.urlencoded({extended: true}))
 
-app.get('/', cors(), (req, res)=> getAllArticles.handleAllArticles(req, res, db))
+app.get('/', (req, res)=> getAllArticles.handleAllArticles(req, res, db))
 app.post('/signup', (req, res)=> signup.handleSignup(req, res, db, bcrypt))	
-app.post('/login', cors(), (req, res)=> login.handleLoginAuth(req, res, db, bcrypt))
+app.post('/login', (req, res)=> login.handleLoginAuth(req, res, db, bcrypt))
 app.get('/profile/:id', auth.requireAuth, (req, res)=> profile.handleProfile(req, res, db))
 app.put('/update_profile/:id',  auth.requireAuth, (req, res)=> updateProfile.handleProfileUpdate(req,res, db))
 app.post('/create_article/:id', auth.requireAuth, (req, res)=> article.createArticle(req, res, db))
